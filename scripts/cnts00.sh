16851ebc6fb06358c938fa8613c51dd70f08e99e
Oscar Garcia <oscarg1677@gmail.com>
2016-07-12 20:57:40 -0600

    Scripts de maquinas virtuales + history de root en lab de LVM

--- scripts/cnts00.sh
+++ scripts/cnts00.sh
@@ -6,7 +6,7 @@ virt-install \
     -r 512 \
     --hvm \
     --disk path=/srv/servidores/cnts00.img,size=5 \
-    --cdrom /home/sysadmin/Downloads/CentOS-7-x86_64-Minimal-1511.iso \
+    --cdrom /home/useradmin/Downloads/CentOS-7-x86_64-Minimal-1511.iso \
     --network network=default \
     --graphics vnc \
     --accelerate
